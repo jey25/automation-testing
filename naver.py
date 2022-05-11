3924998eb396c650d5d96b63a55628d9021e1c5e
jey25 <jjangeoyoung@gmail.com>
2022-05-11 18:05:49 +0900

    6

--- naver.py
+++ naver.py
@@ -53,90 +53,30 @@ browser.get('https://whale.naver.com/ko/whats_new/')
 
 browser.find_element_by_css_selector('#ccContent > article.WhatsNew_contents.Whale_News > div > div > p > a').click()
 
+i = 2
+while i < 36:
+    browser.get(f"https://whale.naver.com/ko/whats_new/press/{i}.html")
+    browser.get('https://whale.naver.com/ko/whats_new/press/')
+    i += 1
 
-browser.find_element_by_css_selector('#press_list > li:nth-child(1) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
+browser.find_element_by_xpath('//*[@id="s_year"]').click()
+browser.find_element_by_xpath('//*[@id="s_year"]/option[2]').click()
+browser.find_element_by_xpath('//*[@id="s_year"]/option[3]').click()
+browser.find_element_by_xpath('//*[@id="s_year"]/option[4]').click()
+browser.find_element_by_xpath('//*[@id="s_year"]/option[5]').click()
+browser.find_element_by_xpath('//*[@id="s_year"]/option[6]').click()
+browser.find_element_by_xpath('//*[@id="s_year"]/option[7]').click()
+browser.find_element_by_xpath('//*[@id="s_year"]').click()
+browser.find_element_by_xpath('//*[@id="s_year"]/option[1]').click()
 
-browser.find_element_by_css_selector('#press_list > li:nth-child(2) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
+browser.find_element_by_xpath('//*[@id="search_str"]').click()
+browser.find_element_by_xpath('//*[@id="search_str"]').send_keys('네이버')
+browser.find_element_by_xpath('//*[@id="ccContent"]/article[3]/div[1]/div[1]/form/fieldset/div/p[2]/button').click()
+time.sleep(2)
 
-browser.find_element_by_css_selector('#press_list > li:nth-child(3) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
+browser.find_element_by_xpath('//*[@id="search_str"]').click()
+browser.find_element_by_xpath('//*[@id="search_str"]').clear()
+browser.find_element_by_xpath('//*[@id="search_str"]').send_keys('asdasd')
+browser.find_element_by_xpath('//*[@id="ccContent"]/article[3]/div[1]/div[1]/form/fieldset/div/p[2]/button').click()
 
-browser.find_element_by_css_selector('#press_list > li:nth-child(4) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(5) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(6) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(7) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(8) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(9) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-
-browser.find_element_by_css_selector('#press_paging > li:nth-child(4) > a').click()
-
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(1) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(2) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(3) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(4) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(5) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(6) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(7) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(8) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(9) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-
-browser.find_element_by_css_selector('#press_paging > li:nth-child(5) > a').click()
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(1) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(2) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(3) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(4) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(5) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(6) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(7) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(8) > a > span.left').click()
-browser.get('https://whale.naver.com/ko/whats_new/press/')
-
-browser.find_element_by_css_selector('#press_list > li:nth-child(9) > a > span.left').click()
+browser.get('https://help.whale.naver.com/desktop/')
