e644b5676f1790561c0de87a19c7054770ddf935
jey25 <jjangeoyoung@gmail.com>
2022-05-28 02:08:03 +0900

    Create UIAutomator.java

---
+++ UIAutomator.java
@@ -0,0 +1,11 @@
+@Test
+
+// 확인 텍스트를 가진 오브젝트를 찾아 permissionConfirmButton 에 넣고,
+// permissionConfirmButton 이 Null 이 아니라면 해당 영역을 클릭한다.
+
+
+fun testInitialization() {
+    val permissionConfirmButton = mDevice.wait(Until.findObject(By.textContains("확인")), 5000)
+    Assert.assertNotNull(permissionConfirmButton)
+    permissionConfirmButton.click()
+}
