f825fb74cd887397e58ad95e039f4de8fbcd58a8
jey25 <jjangeoyoung@gmail.com>
2022-06-28 15:49:00 +0900

    Update k_pay.py

--- k_pay.py
+++ k_pay.py
@@ -1,37 +1,19 @@
-
-# selenium 4.0 이후 최신 버전 사용법 
-
-# 아래 설치 또는 업그레이드
-
-# pip install --upgrade pip
-# pip install --upgrade selenium
-# pip install webdriver_manager
-
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 import time
 
+#main
+browser = webdriver.Chrome('C:\chromedriver\chromedriver.exe')
+browser.get('https://www.kakaopay.com/')
+browser.set_window_size(1650, 950)
 
-# 크롬 드라이버 자동 업데이트
-from webdriver_manager.chrome import ChromeDriverManager
+browser.implicitly_wait(10)
 
+browser.find_element_by_css_selector('#__next > div.css-1okryao.e17yt6xy6 > header > div.css-1mjuj2d.e1ur28bq3 > a').click()
+time.sleep(0.5)
 
-# 브라우저 꺼짐 방지
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
+browser.find_element_by_css_selector('#__next > div.css-1okryao.e17yt6xy6 > header > div.css-ezcqgw.e1ur28bq1 > div:nth-child(1) > a').click()
+time.sleep(0.5)
 
-
-# 불필요한 에러 메시지 없애기
-chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"]) 
-
-
-service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=chrome_options)
-
-driver.get('https://www.kakaopay.com/')
-
-driver.set_window_size(1650, 950)
-driver.implicitly_wait(10)
-
-
+browser.find_element_by_xpath('//*[@id="__next"]/header/div[2]/div[2]/a').click()
+browser.find_element_by_css_selector('#__next > div.css-1qmh5wl.evcs03d0 > div:nth-child(1) > section > div.css-1ysqers.e1256prn4 > div.css-1al2v5r.e1sz0hhq1 > a:nth-child(3) > p').click()
