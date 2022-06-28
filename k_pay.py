1e8c6e6248ebabef0cafbdf20afa3699872464ea
jey25 <jjangeoyoung@gmail.com>
2022-06-28 15:40:24 +0900

    Create k_pay.py

---
+++ k_pay.py
@@ -0,0 +1,37 @@
+
+# selenium 4.0 이후 최신 버전 사용법 
+
+# 아래 설치 또는 업그레이드
+
+# pip install --upgrade pip
+# pip install --upgrade selenium
+# pip install webdriver_manager
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import time
+
+
+# 크롬 드라이버 자동 업데이트
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+# 브라우저 꺼짐 방지
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+
+# 불필요한 에러 메시지 없애기
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"]) 
+
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+driver.get('https://www.kakaopay.com/')
+
+driver.set_window_size(1650, 950)
+driver.implicitly_wait(10)
+
+
